ace609ea36407d165659217fb970dee7f3ce6f48
aida_ <aida_@DESKTOP-DGR3H62>
2021-11-13 18:26:49 +0100

    Apartado 12. Métodos getter y setter color y posicion

--- src/main/java/org/iesalandalus/programacion/torreajedrez/Torre.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Torre.java
@@ -4,5 +4,39 @@ public class Torre {
 	
 	private Color colorTorre;
 	private Posicion posicionTorre;
+	
+	public void setColor(Color colorTorre) { // Método SETTER color
+		
+		if (colorTorre==null) {
+			
+			throw new NullPointerException("ERROR: No se puede asignar un color nulo.");
+			
+		} else { 
+			
+			this.colorTorre=colorTorre;
+		}
+	}
+	
+	public Color getColor() { //Método GETTER color
+		
+		return colorTorre;
+	}
+	
+	public void setPosicion(Posicion posicionTorre) { // Método SETTER posicion
+		
+		if (posicionTorre==null) {
+			
+			throw new NullPointerException ("ERROR: Posición no válida.");
+			
+		} else {
+			
+			this.posicionTorre=posicionTorre;
+		}
+	}
+	
+	public Posicion getPosicion() { // Método GETTER posicion
+		
+		return posicionTorre;
+	}
 
 }
