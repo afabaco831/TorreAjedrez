43611adb256dab59147aab6e5765ad86fb89aada
aida_ <aida_@DESKTOP-DGR3H62>
2021-11-13 18:11:30 +0100

    Apartado 6. Métodos getter y setter posicion

--- src/main/java/org/iesalandalus/programacion/torreajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Posicion.java
@@ -4,5 +4,39 @@ public class Posicion {
 	
 	private int fila;
 	private char columna;
+	
+	public void setFila(int fila) { // Método SETTER fila
+		
+		if (fila<1 || fila>8) {
+			
+			throw new IllegalArgumentException("EROOR: Fila no válida.");
+			
+		} else {
+			
+			this.fila=fila;
+		}
+	}
+	
+	public int getFila() { //Método GETTER fila
+		
+		return fila;
+	}
+	
+	public void setColumna(char columna) { //Método SETTER columna
+		
+		if (columna<'a' || columna>'h') {
+			
+			throw new IllegalArgumentException("ERROR: Columna no válida.");
+			
+		} else {
+			
+			this.columna=columna;
+		}
+	}
+	
+	public char getColumna() { //Método GETTER columna
+		
+		return columna;
+	}
 
 }
