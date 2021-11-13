5663cabf7db74ad6fbb9aaf1f3725531aca55614
aida_ <aida_@DESKTOP-DGR3H62>
2021-11-13 18:17:09 +0100

    Apartado 10. método toString

--- src/main/java/org/iesalandalus/programacion/torreajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Posicion.java
@@ -76,6 +76,11 @@ public class Posicion {
 		Posicion other = (Posicion) obj;
 		return columna == other.columna && fila == other.fila;
 	}
+
+	@Override
+	public String toString() {
+		return "fila=" + fila + ", columna=" + columna;
+	}
 	
 	
 
