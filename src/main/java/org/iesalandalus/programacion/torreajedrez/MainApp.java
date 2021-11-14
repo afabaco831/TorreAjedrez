6b32ea396091c1dd1e809ef772cb1501b1592df2
aida_ <aida_@DESKTOP-DGR3H62>
2021-11-14 12:14:34 +0100

    Apartado 20.2. Método mostrarMenu

--- src/main/java/org/iesalandalus/programacion/torreajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/MainApp.java
@@ -8,6 +8,17 @@ public class MainApp {
 		
 		System.out.println(torre.toString());
 	}
+	
+	private static void mostratMenu() {
+		
+		System.out.println("MENÚ DE SELECCIÓN DE OPCIONES: ");
+		System.out.println("1. Crear torre por defecto");
+		System.out.println("2. Crear torre de un color a elegir.");
+		System.out.println("3. Crear torre de un color y columna inicial a elegir.");
+		System.out.println("4. Mover la torre.");
+		System.out.println("5. Salir.");
+		
+	}
 
 	public static void main(String[] args) {
 		System.out.println("kk");
