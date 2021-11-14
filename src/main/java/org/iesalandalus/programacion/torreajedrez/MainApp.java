6306936b9ec415d0bfbd43c615f85017cd93271b
aida_ <aida_@DESKTOP-DGR3H62>
2021-11-14 12:11:16 +0100

    Apartado 20.1. Método mostrarTorre

--- src/main/java/org/iesalandalus/programacion/torreajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/MainApp.java
@@ -1,6 +1,13 @@
 package org.iesalandalus.programacion.torreajedrez;
 
 public class MainApp {
+	
+	private static Torre torre;
+	
+	private static void mostrarTorre() {
+		
+		System.out.println(torre.toString());
+	}
 
 	public static void main(String[] args) {
 		System.out.println("kk");
